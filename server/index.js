b56510136ebbd67e90194b3d3296dd2fb509673a
Imanda03 <asis03ktm@gmail.com>
2023-01-07 20:55:46 +0545

    server setup and add authentication and authorization

---
+++ server/index.js
@@ -0,0 +1,64 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url"; //THis two help to figure the path of dir
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth";
+import { verifyToken } from "./middleWare/auth.js";
+
+//Configurations                --> to grab the file url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+// app.use(
+//   helmet.permittedCrossDomainPolicies({
+//     permittedPolicies: "none",
+//   })
+// );
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+// Setup File Storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+//Routes with files
+app.post("/auth/register", upload.single("picture"), register);
+
+/* Routes */
+app.use("/auth", authRoutes);
+
+//Starting for database  ------> Mongoose Setup
+// console.log(process.envMONGO_URL);
+const PORT = process.env.PORT || 6001;
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
